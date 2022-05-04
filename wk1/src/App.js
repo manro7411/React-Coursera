295ec4a4179a9913f184aaa10b078543b3c93eb2
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-05-05 00:07:32 +0700

    W1-02 Components Part 1

--- wk1/src/App.js
+++ wk1/src/App.js
@@ -1,33 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react' ;
-
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './MenuComponent';
+class App extends Component {
 
-class App extends Component { 
-
-  render() { 
-
-    return ( 
-
-      <div className="App"> 
-
-        <Navbar dark color="primary"> 
-
-          <div className="container"> 
-
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand> 
-
-          </div> 
-
-        </Navbar> 
-
-      </div> 
-
-    ); 
-
-  } 
-
-} 
-
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container ">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <div className='d-flex col-12 mt-5'>
+        <Menu />
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
