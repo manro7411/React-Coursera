963d87c1bd6b1c5bd7af39e6c874dd566b194e96
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-05-05 13:57:57 +0700

    W1-03 Components Part 2

--- wk1/src/App.js
+++ wk1/src/App.js
@@ -1,22 +1,20 @@
+/* eslint-disable no-unused-expressions */
 import './App.css';
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
-class App extends Component {
+import { DISHES } from './dishes'; 
+class App extends Component { 
 
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container ">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <div className='d-flex col-12 mt-5'>
-        <Menu />
-        </div>
-      </div>
-    );
-  }
-}
+    constructor(props) { 
+  
+      super(props); 
+  
+      this.state 
+  
+        dishes: DISHES 
+  
+      }; 
+  
+    } 
 export default App;
